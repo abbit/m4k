09f99283f0611349a488ffd7298cf30560a9c3b2
Mikhail Kopylov <kopylovmichaelfl@gmail.com>
2025-01-12 21:34:27 +0700

    chore(koreader settings): change zoom mode type to 'full'

--- koreader-customizations/settings/profiles.lua
+++ koreader-customizations/settings/profiles.lua
@@ -1,19 +1,19 @@
 -- we can read Lua syntax here!
 return {
-    ["manga"] = {
-        ["kopt_page_scroll"] = false,
-        ["kopt_writing_direction"] = 1,
-        ["kopt_zoom_direction"] = 0,
-        ["kopt_zoom_mode_type"] = "height",
-        ["settings"] = {
-            ["name"] = "manga",
-            ["order"] = {
-                [1] = "kopt_zoom_mode_type",
-                [2] = "kopt_page_scroll",
-                [3] = "kopt_writing_direction",
-                [4] = "kopt_zoom_direction",
-            },
-            ["registered"] = true,
-        },
-    },
+	["manga"] = {
+		["kopt_page_scroll"] = false,
+		["kopt_writing_direction"] = 1,
+		["kopt_zoom_direction"] = 0,
+		["kopt_zoom_mode_type"] = "full",
+		["settings"] = {
+			["name"] = "manga",
+			["order"] = {
+				[1] = "kopt_zoom_mode_type",
+				[2] = "kopt_page_scroll",
+				[3] = "kopt_writing_direction",
+				[4] = "kopt_zoom_direction",
+			},
+			["registered"] = true,
+		},
+	},
 }
